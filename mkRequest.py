e4fb6ae34927e88d6e67d80c71053fa214fa8186
Malhar Patel <malhar30000@gmail.com>
2025-06-07 19:40:28 +0000

    mkRequest updated; argparse added

--- mkRequest.py
+++ mkRequest.py
@@ -1,34 +1,64 @@
+# Run command: python3 mkRequest.py --dir "name"
+
+# name is the directory inside the Outputs directory which has the Input.json file.
+# It must contain an Input.json file with the JSON Schema for the statements and its proof.
+
 import requests
 import json
 
-url = "http://localhost:7654"
+def main(name):
 
-filenum = "test"
-filename = f"JSON_Outputs/p_{filenum}.json"
+    url = "http://localhost:7654"
 
-with open(filename, "r") as infile:
-    json_output = json.load(infile)
+    # Set the Outputs directory over here
+    direc = f"Outputs_Set2/Responses/{name}"
 
-payload = {
-    "task": "lean_from_json_structured",
-    "json_structured": json_output,
-    "greedy": False
-}
+    input_file = f"{direc}/Input.json"
+    response_file = f"{direc}/Response.json"
+    lean_file = f"{direc}/lean_code.lean"
 
-headers = {
-    "Content-Type": "application/json"
-}
+    with open(input_file, "r") as infile:
+        json_output = json.load(infile)
 
-response = requests.post(url, headers=headers, data=json.dumps(payload))
+    # Set the payload for the request.
+    payload = {
+        "task": "lean_from_json_structured",
+        "json_structured": json_output,
+        "greedy": False
+    }
 
-if response.ok:
-    with open(f"Responses/resp_{filenum}.json", "w") as f:
-        json.dump(response.json(), f, indent=2)
-    print(f"Response saved to resp_{filenum}.json")
-    print("Output Lean code:")
-    print("---------------------")
-    print(response.json()["lean_code"])
-    print("---------------------")
-else:
-    print(f"Request failed with status code {response.status_code}")
-    print("Response text:", response.text)
+    # Set the headers for the request.
+    headers = {
+        "Content-Type": "application/json"
+    }
+
+    response = requests.post(url, headers=headers, data=json.dumps(payload))
+
+    if response.ok:
+        # Dump the response JSON into Response.json in the same directory.
+        with open(response_file, "w") as f:
+            json.dump(response.json(), f, indent=2)
+        print(f"Response saved")
+
+        print("Output Lean code:")
+        print("---------------------")
+        print(response.json()["lean_code"])
+        print("---------------------")
+
+        # Write the output Lean code into lean_code.lean in the same directory.
+        with open(lean_file, "w") as f:
+            f.write("import Mathlib\n\n")
+            f.write(response.json()["lean_code"])
+
+    else:
+        print(f"Request failed with status code {response.status_code}")
+        print("Response text:", response.text)
+
+if __name__ == "__main__":
+    
+    import argparse
+    parser = argparse.ArgumentParser(description="parser")
+    parser.add_argument("--dir", metavar="path", required=True, help="directory where Input.json is")
+    args=parser.parse_args()
+
+    main(args.dir)

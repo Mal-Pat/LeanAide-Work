d62d2626b6df5b035ed13d828ffeb1feff380e20
Malhar Patel <malhar30000@gmail.com>
2025-07-03 12:07:30 +0000

    first run for simple statements

--- Simple/consec3div3/lean_code.lean
+++ Simple/consec3div3/lean_code.lean
@@ -1,8 +1,15 @@
 import Mathlib
-
-theorem three_dvd_sum_consecutive : ∀ (n : ℤ), 3 ∣ n + (n + 1) + (n + 2) :=
+universe u v w u_1 u_2 u_3 u₁ u₂ u₃
+set_option maxHeartbeats 10000000
+set_option linter.unreachableTactic false
+open Nat
+theorem div_three_sum_consecutive_nat : ∀ (n : ℤ), 3 ∣ n + (n + 1) + (n + 2) :=
     by
     intro n
-    have assert_16504161470097087058 : n + (n + 1) + (n + 2) = 3 * (n + 1) := by sorry
+    have assert_16504161470097087058 : n + (n + 1) + (n + 2) = 3 * (n + 1) :=
+      by
+      trace "Automation Tactics hammer [] for goal: n + (n + 1) + (n + 2) = 3 * (n + 1)"
+      sorry
     (expose_names; exact Dvd.intro (n_1 + 1) (id (Eq.symm assert_16504161470097087058)))
-    (omega)
+    trace "Automation Tactics hammer for goal: 3 ∣ n + (n + 1) + (n + 2)"
+    sorry
